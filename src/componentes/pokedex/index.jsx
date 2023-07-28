2a22e62454e6549e0238c3809250ec3a6b070432
Yariserrano12 <135663345+Yariserrano12@users.noreply.github.com>
2023-07-27 22:38:30 -0600

    Update index.jsx

--- src/componentes/pokedex/index.jsx
+++ src/componentes/pokedex/index.jsx
@@ -1,40 +1,12 @@
-import React from "react";
-import Pagination from "../pagination";
-import Pokemon from "../pokemon";
+import React from 'react';
 
-const Pokedex = (props) => {
-  const { pokemons, page, setPage, total, loading } = props;
-
-  const lastPage = () => {
-    const nextPage = Math.max(page - 1, 0);
-    setPage(nextPage);
-  };
-
-  const nextPage = () => {
-    const nextPage = Math.min(page + 1, total - 1);
-    setPage(nextPage);
-  };
-
+const Pokedex = ({ pokemon }) => {
   return (
     <div>
-      <div className="header">
-        <h1>Pokedex</h1>
-        <Pagination
-          page={page + 1}
-          totalPages={total}
-          onLeftClick={lastPage}
-          onRightClick={nextPage}
-        />
-      </div>
-      {loading ? (
-        <div>Cargando pokemones...</div>
-      ) : (
-        <div className="pokedex-grid">
-          {pokemons.map((pokemon, idx) => {
-            return <Pokemon pokemon={pokemon} key={pokemon.name} />;
-          })}
-        </div>
-      )}
+      <h2>{pokemon.name}</h2>
+      <img src={pokemon.imageUrl} alt={pokemon.name} />
+      <p>Tipo: {pokemon.type}</p>
+      {/* Otros detalles del pokémon */}
     </div>
   );
 };
